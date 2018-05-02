da1db48f37a15efd8bb854f8ce2fbe35682a11ae
Alessio Santangelo <alessio.santangelo@prima.it>
2018-05-02 11:35:29 +0200

    Setup

--- src/Achille/View.elm
+++ src/Achille/View.elm
@@ -1,6 +1,11 @@
 module Achille.View exposing (view)
 
-import Achille.Model exposing (AppStatus(..), Model, Msg(..))
+import Achille.Model
+    exposing
+        ( AppStatus(..)
+        , Model
+        , Msg(..)
+        )
 import Html exposing (..)
 import Html.Attributes exposing (class)
 
@@ -9,5 +14,4 @@ view : Model -> Html Msg
 view model =
     div
         []
-        [ text "Achille is angry"
-        ]
+        []
