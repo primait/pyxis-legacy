79b1e5aa5a50077d8983410c0c3fab8af1723db0
francescophra <phragraphics@gmail.com>
2018-07-10 15:50:18 +0200

    browser detection, datepicker, fix edge (#29)

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
+const helpers       = require('./helpers.js')
 const template      = require('./index.html')
 const pyxisStyle    = require('./scss/app.scss')
 const primaStyle    = require('./scss/prima.scss')
 const introImg      = require('./assets/intro.jpg')
 const Elm           = require('./App.elm')
 const route         = window.location.pathname
+const isIE          = helpers.detectIE() <= 11
+const isEdge        = helpers.detectIE() >= 12
+
+isIE ? document.body.className += ' is-IE' : null
+isEdge ? document.body.className += ' is-Edge' : null;
+
+(!!('ontouchstart' in window) || !!('msmaxtouchpoints' in window.navigator)) ? document.body.className += ' isTouchDevice' : null;
 
 const app = Elm.App.embed(document.getElementById('app'), {
   route: route
 })
 
 app.ports.inspectHtml.subscribe(selector => {
-  const htmlSource = [].slice.call(document.querySelectorAll(selector)).map(item => beautifyHtml(item.innerHTML))
+  const htmlSource = [].slice.call(document.querySelectorAll(selector)).map(item => helpers.beautifyHtml(item.innerHTML))
   app.ports.htmlSnippet.send(htmlSource.join(''))
 })
 
@@ -20,7 +28,7 @@ app.ports.copyToClipboard.subscribe(selector => {
   const r = hexBits[0]
   const g = hexBits[1]
   const b = hexBits[2]
-  copyToClipboard(rgbToHex(r, g, b))
+  helpers.copyToClipboard(helpers.rgbToHex(r, g, b))
   app.ports.copied.send(true)
 })
 
@@ -34,11 +42,3 @@ document.addEventListener('click', event => {
     app.ports.clickedOutside.send(true)
   }
 }, false)
-
-const copyToClipboard = str => { const el = document.createElement('textarea'); el.value = str; el.setAttribute('readonly', ''); el.style.position = 'absolute'; el.style.left = '-9999px'; document.body.appendChild(el); el.select(); document.execCommand('copy'); document.body.removeChild(el); }
-
-const rgbToHex = (r, g, b) => '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
-
-const beautifyHtml = str => { const div = document.createElement('div'); div.innerHTML = str.trim(); return formatHtml(div, 0).innerHTML; }
-
-const formatHtml = (node, level) => { let indentBefore = new Array(level++ + 1).join('  '); let indentAfter  = new Array(level - 1).join('  '); let textNode; for(let i = 0; i < node.children.length; i++) { textNode = document.createTextNode('\n' + indentBefore); node.insertBefore(textNode, node.children[i]); formatHtml(node.children[i], level); if (node.lastElementChild == node.children[i]) { textNode = document.createTextNode('\n' + indentAfter); node.appendChild(textNode); } } return node; }
