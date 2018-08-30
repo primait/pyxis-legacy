e86f4b91ecc82722de75412e8910c72e959bc5de
aurcap <38243925+aurcap@users.noreply.github.com>
2018-08-30 15:36:38 +0200

    Tipografia line-height (#68)

--- src/Pyxis/Components/Typography/View.elm
+++ src/Pyxis/Components/Typography/View.elm
@@ -14,7 +14,7 @@ view model =
     let
         renderText : (List (Attribute Msg) -> List (Html Msg) -> Html Msg) -> Html Msg
         renderText tagName =
-            tagName [] [ text "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor." ]
+            tagName [] [ text "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." ]
     in
     [ componentTitle [ text "Typography" ]
     , divider
