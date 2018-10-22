9e7fdcd7de7fd9acf0fb169c625a1a8f527101c4
Aurora Cappello <aurora.cappello@prima.it>
2018-10-22 16:41:39 +0200

    fix statiche-flusso

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ const config = {
         use: [
           { loader: MiniCssExtractPlugin.loader },
           { loader: 'css-loader' },
-          { loader: 'postcss-loader' },
+          { loader: 'postcss-loader', options: { reduceIdents: false } },
           { loader: 'sass-loader' },
         ]
       },
