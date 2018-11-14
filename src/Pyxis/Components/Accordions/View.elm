3c34881d7f201ad7a43678952aab1c1e9ae5c776
Ilaria Micale <ilaria.micale@prima.it>
2018-11-14 09:19:48 +0100

    tupla accordion

--- src/Pyxis/Components/Accordions/View.elm
+++ src/Pyxis/Components/Accordions/View.elm
@@ -1,7 +1,7 @@
 module Pyxis.Components.Accordions.View exposing (view)
 
 import Html exposing (..)
-import Html.Attributes exposing (class, classList)
+import Html.Attributes exposing (class, classList, href)
 import Pyxis.Components.Accordions.Model
     exposing
         ( Model
@@ -19,4 +19,30 @@ view : Model -> List (Html Msg)
 view model =
     [ componentTitle [ text "Accordions" ]
     , divider
+    , componentShowdown "Accordion" "Accordion" InspectHtml [ accordion Nothing ]
+    , componentShowdown "Accordion Dark" "AccordionDark" InspectHtml [ accordion (Just "dark") ]
     ]
+
+
+accordion : Maybe String -> Html Msg
+accordion accordionStyle =
+    ul
+        [ classList
+            [ ( "m-accordion", True )
+            , ( "m-accordion--" ++ Maybe.withDefault "" accordionStyle, True )
+            ]
+        ]
+        (List.map accordionListItem [ ( "ciao/", "Ciao" ), ( "ciao/", "Ciao" ) ])
+
+
+accordionListItem : ( String, String, Int ) -> Html Msg
+accordionListItem ( accordionLink, accordionText ) =
+    li
+        [ class "a-accordion__item" ]
+        [ a
+            [ href accordionLink, class "a-link" ]
+            [ text accordionText
+            , i [ class "a-icon" ] []
+            ]
+        , div [ class "a-accordion__item__content" ] []
+        ]
