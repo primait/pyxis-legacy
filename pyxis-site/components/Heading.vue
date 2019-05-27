38158b877dfcd2ac8837e8af54e3435c39978f44
Ivan Gori <ivan.gori@prima.it>
2019-05-27 12:02:27 +0200

    Base Head/Sidebar structure

---
+++ pyxis-site/components/Heading.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Heading component
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Heading'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+</style>
---
+++ pyxis-site/components/Sidebar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Sidebar component
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+
+</style>
