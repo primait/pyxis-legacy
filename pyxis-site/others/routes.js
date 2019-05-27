79572581d3435839850161fb6f48cca7b55e2a15
Alessio Santangelo <alessio.santangelo@prima.it>
2019-05-27 12:19:41 +0200

    Routes

---
+++ pyxis-site/others/routes.js
@@ -0,0 +1,7 @@
+const routes = [
+    { path: '/colors', label: 'Colors' },
+    { path: '/buttons', label: 'Buttons' },
+    { path: '/typography', label: 'Typography' }
+]
+
+export default routes
