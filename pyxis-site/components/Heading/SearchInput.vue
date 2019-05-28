f07485515e98c4632df1782b37c27d1397ecf000
Francesco Prisco <francesco.prisco@prima.it>
2019-05-28 10:18:14 +0200

    styled search input

--- pyxis-site/components/Heading/SearchInput.vue
+++ pyxis-site/components/Heading/SearchInput.vue
@@ -1,5 +1,7 @@
 <template>
-  <input v-on:input='input' type="text" class="heading__search" :placeholder="placeholder" :value="inputValue">
+  <div class="heading__search__wrapper">
+    <input v-on:input='input' type="text" class="heading__search" :placeholder="placeholder" :value="inputValue">
+  </div>
 </template>
 
 <script>
@@ -33,20 +35,36 @@ export default {
 <style lang="scss">
 @import "@/assets/sass/app.scss";
 
+$iconSize: 15px;
+
+.heading__search__wrapper {
+  display: none;
+  align-items: center;
+  margin-left: auto;
+  position: relative;
+
+  &:before {
+    content: url('../../assets/icons/search.svg');
+    height: $iconSize;
+    left: $iconSize + 5px;
+    line-height: $iconSize;
+    position: absolute;
+    width: $iconSize;
+  }
+
+  @include mq(small) {
+    display: flex;
+  }
+}
+
 .heading__search {
   border: none;
   border-radius: 100px;
   background-color: rgba(255,255,255,0.1);
   color: color(textAlt, light);
-  display: none;
   height: 40px;
-  margin-left: auto;
-  padding: 0 20px;
+  padding: 0 20px 0 (20px + $iconSize * 2);
   width: 250px;
-
-  @include mq(small) {
-    display: block;
-  }
 
   &::placeholder {
     color: color(textAlt, light);
