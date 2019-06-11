613e0fa5d15c491cfffc5724227cefe5a430a6e4
Alessio Santangelo <alessio.santangelo@prima.it>
2019-06-11 14:57:26 +0200

    Utility classes splitted

--- pyxis-site/views/Classes.vue
+++ pyxis-site/views/Classes.vue
@@ -6,8 +6,34 @@
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur in rerum amet modi nobis maxime autem? Et, iure tempora libero dolorem soluta ipsum, quas vero veritatis ea debitis aut ut.</p>
     </text-block>
 
+    <hr/>
+    <h4>Design</h4>
     <div class="rules">
-      <div class="rules__item fsXsmall" v-highlight :key="index" v-for="(match, index) in matches">
+      <div class="rules__item fsXsmall" v-highlight :key="index" v-for="(match, index) in matches.design">
+        {{match}}
+      </div>
+    </div>
+    <hr/>
+
+    <h4>Colors</h4>
+    <div class="rules">
+      <div class="rules__item fsXsmall" v-highlight :key="index" v-for="(match, index) in matches.colors">
+        {{match}}
+      </div>
+    </div>
+    <hr/>
+
+    <h4>Fonts</h4>
+    <div class="rules">
+      <div class="rules__item fsXsmall" v-highlight :key="index" v-for="(match, index) in matches.fonts">
+        {{match}}
+      </div>
+    </div>
+    <hr/>
+
+    <h4>Others</h4>
+    <div class="rules">
+      <div class="rules__item fsXsmall" v-highlight :key="index" v-for="(match, index) in matches.others">
         {{match}}
       </div>
     </div>
@@ -46,10 +72,7 @@ import TextBlock from '@/components/TextBlock.vue'
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import styles from '!css-loader!sass-loader?modules!@/assets/sass/utilityClasses.scss'
 
-const pyxisStyle = styles[0][1]
-const regExp = /^\.(?![amo]-)[\w-]+/igm
-const matches = []
-pyxisStyle.replace(regExp, (match, g1, g2) => { matches.push(match) })
+const toCamelCase = str => str.split('-').map(s => s[0].toUpperCase() + s.slice(1)).join('')
 
 export default {
   name: 'Classes',
@@ -57,9 +80,65 @@ export default {
     Container,
     TextBlock
   },
-  data: function () {
-    return {
-      matches: matches.sort()
+  computed: {
+    matches: function () {
+      const pyxisStyle = styles[0][1]
+      const regExp = /^\.(?![amo]-)[\w-]+/igm
+      let rules = []
+      const matches = {
+        design: [],
+        colors: [],
+        fonts: [],
+        others: []
+      }
+
+      pyxisStyle.replace(regExp, (match, g1, g2) => { rules.push(match) })
+
+      rules = rules.map(item => toCamelCase(item))
+
+      matches.design = rules.filter(item => {
+        return (
+          item.startsWith('.align') ||
+          item.startsWith('.border') ||
+          item.startsWith('.line') ||
+          item.startsWith('.no') ||
+          item.startsWith('.overflow') ||
+          item.startsWith('.lowercase') ||
+          item.startsWith('.sticky') ||
+          item.startsWith('.underline') ||
+          item.startsWith('.uppercase') ||
+          item.startsWith('.word')
+        )
+      })
+
+      matches.colors = rules.filter(item => {
+        return (
+          item.startsWith('.bg') ||
+          item.startsWith('.c')
+        )
+      })
+
+      matches.fonts = rules.filter(item => {
+        return (
+          item.startsWith('.fs') ||
+          item.startsWith('.fw')
+        )
+      })
+
+      matches.others = rules.filter(item => {
+        return (
+          !matches.colors.includes(item) &&
+          !matches.design.includes(item) &&
+          !matches.fonts.includes(item)
+        )
+      })
+
+      return {
+        design: [...new Set(matches.design.sort())],
+        colors: [...new Set(matches.colors.sort())],
+        fonts: [...new Set(matches.fonts.sort())],
+        others: [...new Set(matches.others.sort())]
+      }
     }
   }
 }
