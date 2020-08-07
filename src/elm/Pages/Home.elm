59e75acf14dc416dbe229614a4b063a95528298e
Antonio Bonfitto <antonio.bonfitto@prima.it>
2020-08-07 18:01:56 +0200

    Add base homepage content and structure

--- src/elm/Pages/Home.elm
+++ src/elm/Pages/Home.elm
@@ -1,6 +1,6 @@
 module Pages.Home exposing (view)
 
-import Html exposing (Html, a, div, h1, img, p, section, text)
+import Html exposing (Html, a, dd, div, dl, dt, h1, img, p, section, text)
 import Html.Attributes as Attr exposing (alt, class, src, style)
 import Html.Events exposing (onClick)
 import Model exposing (Model, Msg(..))
@@ -15,12 +15,16 @@ view ({ t } as model) =
         [ section []
             [ div [ class "box box--jumbotron" ]
                 [ div [ class "box__content" ]
-                    [ div [] [ img [ src "public/logo-pyxis.png", style "filter" "grayscale(1)", style "width" "150px" ] [] ]
+                    [ div [ class "mq-mobile-hidden" ] [ img [ src "public/logo-pyxis.png", style "filter" "grayscale(1)", style "width" "150px" ] [] ]
                     , p [] [ text <| t "home.pyxis-description" [] ]
                     ]
                 ]
             ]
         , section []
+            [ h1 [] [ text "Progetta, sviluppa, racconta" ]
+            , p [] [ text "Con Pyxis hai tutto quello che ti serve per semplificare il tuo lavoro" ]
+            ]
+        , section [ class "flex-container" ]
             [ viewNavBox
                 { icon = "1"
                 , title = "Design"
@@ -42,6 +46,63 @@ view ({ t } as model) =
                 , linkText = "Scopri"
                 , linkTo = Nothing
                 }
+            ]
+        , section [ class "flex-container" ]
+            [ div []
+                [ h1 [] [ text "Più veloce, più sicuro, più coerente" ]
+                , p []
+                    [ text """
+                        Un design system non è una zavorra di vincoli e
+                        condizioni: è un bagaglio di strumenti e informazioni che ci
+                        permette di osare di più senza perdere di vista l’obiettivo."""
+                    ]
+                , dl []
+                    [ dt [] [ text "Modelli, esempi, riferimenti" ]
+                    , dd [] [ text "Permette di creare documentazione utile in ogni situazione." ]
+                    , dt [] [ text "Tempo ed energia" ]
+                    , dd [] [ text "Velocizza il lavoro e aiuta a concentrarsi sugli aspetti importanti." ]
+                    , dt [] [ text "Cultura collettiva" ]
+                    , dd [] [ text "Genera e diffonde conoscenza, per tutti e a tutti i livelli." ]
+                    ]
+                ]
+            , div []
+                [ img [ alt "placeholder image" ] [] ]
+            ]
+        , section [ class "flex-container flex-container--reversed" ]
+            [ div []
+                [ h1 [] [ text "Quello che facciamo, lo facciamo insieme" ]
+                , p []
+                    [ text """
+                        Pyxis è l’unica fonte di risorse e documentazione per
+                        tutti i processi di design, sviluppo front-end e
+                        creazione di contenuti. Somiglia a noi e al nostro modo
+                        di pensare:"""
+                    ]
+                , dl []
+                    [ dt [] [ text "Semplice" ]
+                    , dd [] [ text "È uno strumento efficace ed essenziale, chiaro e alla portata di tutti." ]
+                    , dt [] [ text "Scalabile" ]
+                    , dd [] [ text "È composto da soluzioni adattabili a ogni esigenza di business." ]
+                    , dt [] [ text "Originale" ]
+                    , dd [] [ text "È innovativo per il suo settore e non teme di anticipare la concorrenza." ]
+                    ]
+                ]
+            , div []
+                [ img [ alt "placeholder image 2" ] [] ]
+            ]
+        , section []
+            [ div [ class "box box--jumbotron" ]
+                [ div [ class "box__content" ]
+                    [ h1 [] [ text """Design e necessità di business sono
+                        conseguenza uno dell’altra, perché la coerenza tra
+                        loro è parte di una precisa strategia.""" ]
+                    , p []
+                        [ text """L’immagine che diamo di noi ha
+                                bisogno di cura e attenzione, perché il modo in
+                                cui ci mostriamo è parte del nostro brand e della nostra identità."""
+                        ]
+                    ]
+                ]
             ]
         ]
 
