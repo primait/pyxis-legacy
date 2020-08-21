93c38c893420ca023c4a7cef51c3993f64ac82eb
Antonio Bonfitto <antonio.bonfitto@prima.it>
2020-08-21 15:38:34 +0200

    Add container showcase page

--- src/elm/Pages/Container.elm
+++ src/elm/Pages/Container.elm
@@ -6,20 +6,26 @@ module Pages.Container exposing
     , view
     )
 
+import Commons.Box as Box
+import Components.ComponentViewer as ComponentViewer
 import Dict
-import Helpers as H
-import Html exposing (Html, div)
+import Helpers as H exposing (Translator, WithTranslator)
+import Html exposing (Html, div, p, text)
 import Html.Attributes exposing (class)
 import Pages.Component as ComponentPage exposing (WithCodeInspectors)
+import Prima.Pyxis.Container as Container
+import Prima.Pyxis.Helpers as PyxisHelpers
 
 
 type alias Model =
-    WithCodeInspectors {}
+    WithCodeInspectors (WithTranslator {})
 
 
-init : Model
-init =
-    { inspectMode = Dict.empty }
+init : Translator -> Model
+init translate =
+    { inspectMode = Dict.empty
+    , translate = translate
+    }
 
 
 
@@ -49,12 +55,95 @@ update msg model =
 
 
 view : Model -> Html Msg
-view model =
+view ({ translate } as model) =
     div [ class "container-page" ]
         [ ComponentPage.view
-            { title = "Container"
-            , description = "Page under construction"
+            { title = translate [] "container-page.title"
+            , description = translate [] "container-page.description"
             , specs = Nothing
-            , sections = []
+            , sections =
+                [ normalContainerSection model
+                , fluidContainerSection model
+                ]
             }
         ]
+
+
+normalContainerSection : Model -> ComponentPage.SectionViewConfig Msg
+normalContainerSection ({ translate } as model) =
+    { title = translate [] "container-page.normal-containers.title"
+    , suggestions = Nothing
+    , content =
+        [ p [] [ Html.text <| translate [] "container-page.normal-containers.description" ]
+        , ComponentViewer.view
+            { id = "row"
+            , isCodeVisible = ComponentPage.isInspecting "row" model
+            , onTogglePreview = ToggleInspect
+            , boxType = Box.Light
+            , label = "row container"
+            , example = "TODO"
+            }
+            [ viewExampleContainer Container.row
+            ]
+        , ComponentViewer.view
+            { id = "column"
+            , isCodeVisible = ComponentPage.isInspecting "column" model
+            , onTogglePreview = ToggleInspect
+            , boxType = Box.Light
+            , label = "column container"
+            , example = "TODO"
+            }
+            [ viewExampleContainer Container.column
+            ]
+        ]
+    }
+
+
+fluidContainerSection : Model -> ComponentPage.SectionViewConfig Msg
+fluidContainerSection ({ translate } as model) =
+    { title = translate [] "container-page.fluid-containers.title"
+    , suggestions = Nothing
+    , content =
+        [ p [] [ Html.text <| translate [] "container-page.fluid-containers.description" ]
+        , ComponentViewer.view
+            { id = "row-fluid"
+            , isCodeVisible = ComponentPage.isInspecting "row-fluid" model
+            , onTogglePreview = ToggleInspect
+            , boxType = Box.Light
+            , label = "row container fluid"
+            , example = "TODO"
+            }
+            [ viewExampleContainer Container.row
+            ]
+        , ComponentViewer.view
+            { id = "column-fluid"
+            , isCodeVisible = ComponentPage.isInspecting "column-fluid" model
+            , onTogglePreview = ToggleInspect
+            , boxType = Box.Light
+            , label = "column container fluid"
+            , example = "TODO"
+            }
+            [ viewExampleContainer Container.column
+            ]
+        ]
+    }
+
+
+viewExampleContainer : Container.Config msg -> Html msg
+viewExampleContainer config =
+    config
+        |> Container.withClass "container--debug"
+        |> Container.withContent exampleContent
+        |> Container.render
+
+
+exampleContent : List (Html msg)
+exampleContent =
+    List.range 1 3
+        |> List.map
+            (\i ->
+                div []
+                    [ p [] [ text <| "Content Area " ++ String.fromInt i ]
+                    , p [] [ Html.text PyxisHelpers.loremIpsum ]
+                    ]
+            )
